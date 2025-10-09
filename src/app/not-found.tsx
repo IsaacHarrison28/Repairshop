f85e8ec363e2f0853fb6ad56f58434ceac2ab246
IsaacHarrison28 <sateonlineservices@gmail.com>
2025-10-09 20:17:17 +0200

    added sentry error tracking

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+  description: "This page could not be found.",
+};
+
+export default function NotFound() {
+  return (
+    <div className="px-2 w-full">
+      <div className="mx-auto flex flex-col items-center justify-center gap-4 mt-20">
+        <h2 className="text-2xl">Page Not Found!</h2>
+        <Image
+          src={"/images/not-found-1024x1024.png"}
+          className="m-0 rounded-xl"
+          alt="404 - Page Not Found"
+          width={300}
+          height={300}
+          sizes="300px"
+          priority={true}
+          title="Page Not Found"
+        />
+      </div>
+    </div>
+  );
+}
